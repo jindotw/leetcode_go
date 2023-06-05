a81c63ade79ce4956a8bdc6fb6e54eebdc379522
typaul1 <typaul1@qq.com>
2023-06-05 18:58:11 +0800

    feat: solved 345.Reverse-Vowels-of-a-String [easy]

--- leetcode/leetcode75/0345.Reverse-Vowels-of-a-String /ReverseVowelsOfAString.go
+++ leetcode/leetcode75/0345.Reverse-Vowels-of-a-String /ReverseVowelsOfAString.go
@@ -1,9 +1,36 @@
 package main
 
+import "fmt"
+
 func reverseVowels(s string) string {
-	return ""
+	r := []rune(s)
+	lft := 0
+	rgt := len(s) - 1
+
+	for lft < rgt {
+		if !isVowel(r[lft]) {
+			lft++
+			continue
+		}
+
+		if !isVowel(r[rgt]) {
+			rgt--
+			continue
+		}
+
+		r[lft], r[rgt] = r[rgt], r[lft]
+		lft++
+		rgt--
+	}
+	return string(r)
+}
+
+func isVowel(ch rune) bool {
+	return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'
 }
 
 func main() {
-
+	s := "hello"
+	reversed := reverseVowels(s)
+	fmt.Println(s, "=>", reversed)
 }
