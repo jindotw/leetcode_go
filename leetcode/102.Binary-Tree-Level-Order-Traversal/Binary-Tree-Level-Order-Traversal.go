ebbb480ca3118d3ca3815757cd1c0aed2e29961a
Paul <typaul1@qq.com>
2023-06-08 17:19:23 +0800

    feat: solved 102. Binary-Tree-Level-Order-Traversal [medium]

---
+++ leetcode/102.Binary-Tree-Level-Order-Traversal/Binary-Tree-Level-Order-Traversal.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"leetcode_go/leetcode/schema"
+)
+
+func levelOrder(root *schema.TreeNode) [][]int {
+	var queue []*schema.TreeNode
+	var result [][]int
+	if root != nil {
+		queue = append(queue, root)
+	}
+
+	for len(queue) > 0 {
+		size := len(queue)
+		var row []int
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node == nil {
+				continue
+			}
+			row = append(row, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		result = append(result, row)
+	}
+
+	return result
+}
+
+func main() {
+	node1 := schema.TreeNode{Val: 1}
+	node3 := schema.TreeNode{Val: 3}
+	node2 := schema.TreeNode{Val: 2, Left: &node1, Right: &node3}
+	result := levelOrder(&node2)
+	fmt.Println(result)
+}
