3d3e874db5fa3f76cb4037ba6386bc4b923d26d9
Paul <typaul1@qq.com>
2023-06-09 11:55:47 +0800

    feat: added extra method and MakeTree() support

--- leetcode/schema/tree.go
+++ leetcode/schema/tree.go
@@ -6,6 +6,18 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func (node *TreeNode) GetLeft() *TreeNode {
+	return node.Left
+}
+
+func (node *TreeNode) GetRight() *TreeNode {
+	return node.Right
+}
+
+func (node *TreeNode) GetVal() int {
+	return node.Val
+}
+
 func MakeTree() *TreeNode {
 	node33 := &TreeNode{Val: 3}
 	node13 := &TreeNode{Val: 1}
@@ -17,4 +29,29 @@ func MakeTree() *TreeNode {
 	node31 := &TreeNode{Val: 3, Left: node12, Right: node42}
 
 	return node31
+}
+
+func MakeTreePathSum() *TreeNode {
+	node41 := &TreeNode{Val: 3}
+	node42 := &TreeNode{Val: -2}
+	node43 := &TreeNode{Val: 1}
+
+	node31 := &TreeNode{3, node41, node42}
+	node32 := &TreeNode{2, nil, node43}
+	node33 := &TreeNode{11, nil, nil}
+
+	node21 := &TreeNode{5, node31, node32}
+	node22 := &TreeNode{-3, nil, node33}
+
+	return &TreeNode{10, node21, node22}
+}
+
+func PreOrderRecursion(node *TreeNode, val *[]int) {
+	if node == nil {
+		return
+	}
+
+	*val = append(*val, node.GetVal())
+	PreOrderRecursion(node.GetLeft(), val)
+	PreOrderRecursion(node.GetRight(), val)
 }
