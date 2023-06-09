c6ca80029d1e0665ffd76b40246318fe1aaec007
Paul <typaul1@qq.com>
2023-06-09 11:56:29 +0800

    feat: solved 437. Path-Sum-III [medium]

---
+++ leetcode/437.Path-Sum-III/Path-Sum-III.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"leetcode_go/leetcode/schema"
+)
+
+func preOrder(node *schema.TreeNode, targetSum int, cache []int) int {
+	if node == nil {
+		return 0
+	}
+	cache = append(cache, node.Val)
+	sum := 0
+	count := 0
+	for i := len(cache) - 1; i >= 0; i-- {
+		sum += cache[i]
+		if targetSum == sum {
+			count++
+		}
+	}
+	count += preOrder(node.Left, targetSum, cache)
+	count += preOrder(node.Right, targetSum, cache)
+
+	return count
+}
+
+func pathSum(root *schema.TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+
+	return preOrder(root, targetSum, []int{})
+}
+
+func main() {
+	root := schema.MakeTreePathSum()
+	count := pathSum(root, 8)
+	fmt.Println("There are", count, "paths")
+}
