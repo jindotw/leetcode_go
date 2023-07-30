74bf2b818223220a26684ad9206e4abd457cf14b
Paul <jindotw@gmail.com>
2023-07-30 15:31:42 +0800

    feat: practiced dynamic programming using Fibonacci Sequence as example

---
+++ experiment/dynamic_programming/fibonacci_seq.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fibonacciSequence(n int) int {
+	if n <= 1 {
+		return n
+	}
+	memo := [2]int{0, 1}
+
+	for i := 2; i <= n; i++ {
+		memo[0], memo[1] = memo[1], memo[0]+memo[1]
+	}
+
+	return memo[1]
+}
+
+func main() {
+	fmt.Println(fibonacciSequence(19))
+}
