00156d9ac9c5f68a9fd9b55e09921199c2541196
Paul <jindotw@gmail.com>
2023-08-15 10:56:42 +0800

    refactor: revised 53. Maximum Subarray [medium]

--- leetcode/0053.Maximum-Subarray/MaximumSubarray.go
+++ leetcode/0053.Maximum-Subarray/MaximumSubarray.go
@@ -20,8 +20,29 @@ func maxSubArray(nums []int) int {
 	return maxSum
 }
 
+func maxSubArray2(nums []int) int {
+	maxHere, maxSoFar := nums[0], nums[0]
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	for _, num := range nums[1:] {
+		maxHere = max(num, maxHere+num)
+		maxSoFar = max(maxHere, maxSoFar)
+	}
+
+	return maxSoFar
+}
+
 func main() {
-	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	// nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	nums := []int{1, 2, 3, -5, 4, 5, 6}
 	maxSum := maxSubArray(nums)
+	maxSum2 := maxSubArray2(nums)
 	fmt.Println("maxSum=", maxSum)
+	fmt.Println("maxSum2=", maxSum2)
 }
