e1d431cde90d67969c7e557bcece697dd0ad15bb
Paul <jindotw@gmail.com>
2023-09-19 01:57:04 +0800

    feat: solved 714 Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee [medium]

--- leetcode/dynamic_programming/0714.Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee/Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.go
+++ leetcode/dynamic_programming/0714.Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee/Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.go
@@ -19,46 +19,66 @@ func dump(memo [][]int) {
 }
 
 func maxProfit(prices []int, fee int) int {
+	/*
+	  memo is a two-dimensional array used for memoization. It keeps track of the maximum profit you can have at each day
+	  and for two possible states:
+	    - whether you hold a stock (memo[i][0]) or
+	    - whether you don't hold a stock (memo[i][1])
+
+	  In simple terms, memo[i][0] represents the maximum profit at the end of day i if you hold a stock on that day,
+	  and memo[i][1] represents the maximum profit at the end of day i if you don't hold a stock. The dynamic
+	  programming approach keeps track of these two states to find the overall maximum profit considering the
+	  transaction fee.
+	*/
 	memo := make([][]int, len(prices))
 	for i := range memo {
 		memo[i] = make([]int, 2)
 	}
+	/*
+		memo[0][0] is initialized to -prices[0], representing that on the first day, you can choose to buy a stock (hence
+		the negative value, as it represents an expense).
+	*/
 	memo[0][0] = -prices[0]
-	memo[0][1] = 0
 
+	/*
+		The loop starts from the second day (i = 1) and iterates through the entire list of stock prices.
+
+		memo[i][0] is updated with the maximum between:
+		  - memo[i-1][0]: Representing the maximum profit if you continue holding a stock from the previous day.
+		  - memo[i-1][1] - Representing the profit gained from selling the stock on the previous day to buy a stock
+			at the price on the current day (prices[i]).
+		memo[i][1] is updated with the maximum between:
+		  - memo[i-1][1]: Representing the maximum profit if you continue not holding a stock (i.e., you didn't sell on the
+			previous day).
+		  - memo[i-1][0] + prices[i] -  Representing the profit after selling the stock on the current day and
+			accounting for the transaction fee.
+	*/
 	for i := 1; i < len(prices); i++ {
-		fmt.Printf("prices[%d] = %d\n", i, prices[i])
-		memo[i][0] = max(memo[i-1][0], -prices[i])
-		memo[i][1] = max(memo[i-1][1], prices[i]+memo[i-1][0])
-		dump(memo)
-		fmt.Println()
+		memo[i][0] = max(memo[i-1][0], memo[i-1][1]-prices[i])
+		memo[i][1] = max(memo[i-1][1], memo[i-1][0]+prices[i]-fee)
 	}
 
-	return 0
+	dump(memo)
+
+	return memo[len(prices)-1][1]
 }
 
-func maxProfit2(prices []int) int {
-	cost := prices[0]
-	profit := 0
+func maxProfit2(prices []int, fee int) int {
+	hold, free := -prices[0], 0
 
-	min := func(a, b int) int {
-		if a > b {
-			return b
-		}
-		return a
+	for i := 1; i < len(prices); i++ {
+		hold, free = max(hold, free-prices[i]), max(free, hold+prices[i]-fee)
 	}
 
-	for _, price := range prices {
-		cost = min(cost, price)
-		profit = max(profit, price-cost)
-	}
+	return free
 
-	return profit
 }
 
 func main() {
 	prices := []int{1, 3, 2, 8, 4, 9}
 	fee := 2
+	// prices := []int{1, 3, 7, 5, 10, 3}
+	// fee := 3
 	fmt.Println(maxProfit(prices, fee))
-	fmt.Println(maxProfit2(prices))
+	fmt.Println(maxProfit2(prices, fee))
 }
