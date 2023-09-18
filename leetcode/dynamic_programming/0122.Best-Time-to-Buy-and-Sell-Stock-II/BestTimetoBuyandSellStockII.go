d03c45134cfa7860a5727e730463d236259bf9a7
Paul <jindotw@gmail.com>
2023-09-19 01:56:54 +0800

    feat: added solution using dynamic programming for Best Time To Buy and Sell Stock II

--- leetcode/dynamic_programming/0122.Best-Time-to-Buy-and-Sell-Stock-II/BestTimetoBuyandSellStockII.go
+++ leetcode/dynamic_programming/0122.Best-Time-to-Buy-and-Sell-Stock-II/BestTimetoBuyandSellStockII.go
@@ -14,7 +14,33 @@ func maxProfit(prices []int) int {
 	return profit
 }
 
+func maxProfit2(prices []int) int {
+	memo := make([][]int, len(prices))
+	for i := range memo {
+		memo[i] = make([]int, 2)
+	}
+	memo[0][0] = -prices[0]
+	memo[0][1] = 0
+
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	for i := 1; i < len(prices); i++ {
+		memo[i][0] = max(memo[i-1][0], memo[i-1][1]-prices[i])
+		memo[i][1] = max(memo[i-1][1], prices[i]+memo[i-1][0])
+		fmt.Println(memo)
+	}
+
+	return 0
+}
+
 func main() {
-	profit := maxProfit([]int{7, 1, 5, 3, 6, 4})
+	prices := []int{7, 1, 5, 3, 6, 4}
+	profit := maxProfit(prices)
 	fmt.Println("Profit=", profit)
+	maxProfit2(prices)
 }
