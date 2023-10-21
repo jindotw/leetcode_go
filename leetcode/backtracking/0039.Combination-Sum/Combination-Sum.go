b9b7d08e58d1ab90ddeaeaf00133e2f199e8c3c2
Paul <jindotw@gmail.com>
2023-10-21 22:48:09 +0800

    refactor: added one more solution to 39 Combination-Sum [medium]

--- leetcode/backtracking/0039.Combination-Sum/Combination-Sum.go
+++ leetcode/backtracking/0039.Combination-Sum/Combination-Sum.go
@@ -28,10 +28,42 @@ func combinationSum(candidates []int, target int) [][]int {
 	return results
 }
 
+func combinationSum2(candidates []int, target int) [][]int {
+	results := make([][]int, 0)
+	st := make([]int, 0)
+	var dfs func(int, int)
+	dfs = func(start, sum int) {
+		if target == sum {
+			tmp := make([]int, len(st))
+			copy(tmp, st)
+			results = append(results, tmp)
+			return
+		}
+		if start >= len(candidates) || sum > target {
+			return
+		}
+
+		st = append(st, candidates[start])
+		dfs(start, sum+candidates[start])
+		st = st[:len(st)-1]
+
+		dfs(start+1, sum)
+	}
+
+	dfs(0, 0)
+
+	return results
+}
+
 func main() {
 	candidates := []int{2, 3, 6, 7}
 	target := 7
 	results := combinationSum(candidates, target)
+	for _, res := range results {
+		fmt.Println(res)
+	}
+	fmt.Println()
+	results = combinationSum2(candidates, target)
 	for _, res := range results {
 		fmt.Println(res)
 	}
